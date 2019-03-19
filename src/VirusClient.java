3291b213785f199b5e767b149c52b64ede664fac
Tie C <usbtyping@gmail.com>
2019-03-19 14:04:19 -0700

    Finish Checkpoint 2

--- src/VirusClient.java
+++ src/VirusClient.java
@@ -7,10 +7,101 @@ import java.util.Scanner;
 public class VirusClient {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		ArrayList<Virus> vArray = parse();
+		
+		boolean running = true;
+		
+		System.out.println(" __      ___                  _____        _        _                    \r\n" + 
+				" \\ \\    / (_)                |  __ \\      | |      | |                   \r\n" + 
+				"  \\ \\  / / _ _ __ _   _ ___  | |  | | __ _| |_ __ _| |__   __ _ ___  ___ \r\n" + 
+				"   \\ \\/ / | | '__| | | / __| | |  | |/ _` | __/ _` | '_ \\ / _` / __|/ _ \\\r\n" + 
+				"    \\  /  | | |  | |_| \\__ \\ | |__| | (_| | || (_| | |_) | (_| \\__ \\  __/\r\n" + 
+				"     \\/   |_|_|   \\__,_|___/ |_____/ \\__,_|\\__\\__,_|_.__/ \\__,_|___/\\___|\r\n" + 
+				"                                                                         \r\n" + 
+				"                                                                         ");
+		
+		System.out.println();
+		
+		
+
+		
+		while(running) {
+			VirusCollection vcollection = new VirusCollection(vArray);
+			enterCommand();
+			
+			Scanner s = new Scanner(System.in);
+			String input = s.next();
+			
+			if(input.equals("list")) {
+				vcollection.list();
+			} else if(input.toLowerCase().equals("help")) {
+				System.out.println();
+				System.out.println();
+				
+				System.out.println("888    888          888              \r\n" + 
+						"888    888          888              \r\n" + 
+						"888    888          888              \r\n" + 
+						"8888888888  .d88b.  888 88888b.  d8b \r\n" + 
+						"888    888 d8P  Y8b 888 888 \"88b Y8P \r\n" + 
+						"888    888 88888888 888 888  888     \r\n" + 
+						"888    888 Y8b.     888 888 d88P d8b \r\n" + 
+						"888    888  \"Y8888  888 88888P\"  Y8P \r\n" + 
+						"                        888          \r\n" + 
+						"                        888          \r\n" + 
+						"                        888          \r\n" + 
+						"                                                 \r\n" + 
+						"888888 888888 888888 888888 888888\r\n");
+				System.out.println();
+				System.out.println("list: lists all files in directory.");
+				System.out.println("Filter [Gene] [DNA,RNA]: lists all files in directory.");
+				System.out.println();
+				System.out.println();
+			} else if(input.toLowerCase().equals("sort")) {
+				input = s.next();
+				if(input.toLowerCase().equals("gene")) {
+					vcollection.sortGene();
+				} else {
+					System.out.println("Invalid Input");
+				}
+			} else if(input.toLowerCase().equals("filter")) {
+				input = s.next();
+				if(input.toLowerCase().equals("gene")) {
+					input = s.next();
+					if(input.toLowerCase().equals("dna")) {
+						vcollection.filterGene("DNA");
+					} else if(input.toLowerCase().equals("rna")) {
+						vcollection.filterGene("RNA");
+					} else {
+						System.out.println("Invalid Input");
+					}
+				} else {
+					System.out.println("Invalid Input");
+				}
+			}
+		}
+	}
+	
+	public static void topShell() {
+		System.out.println("_");
+	}
+	
+	public static void bottomShell() {
+		
+	}
+	
+	public static void enterCommand() {
+		System.out.println("Enter Command(Type 'help' for a list of commands): ");
+	}
+	public static ArrayList<Virus> parse() {
 		File[] dirFiles = new File("files/").listFiles();
-		ArrayList<Virus> vArray = new ArrayList<Virus>();
+		ArrayList<Virus> array = new ArrayList<Virus>();
 		for(int i = 0; i<dirFiles.length; i++) {
-			Scanner parse = new Scanner(dirFiles[i]);
+			Scanner parse = null;
+			try {
+				parse = new Scanner(dirFiles[i]);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 			parse.next();
 			parse.next();
 			int bp = parse.nextInt();
@@ -37,109 +128,19 @@ public class VirusClient {
 					def = def + " " + parse.next();
 				}
 			}
-			parse = new Scanner(dirFiles[i]).useDelimiter("ORIGIN");
+			try {
+				parse = new Scanner(dirFiles[i]).useDelimiter("ORIGIN");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 			parse.next();
 			String origin = parse.next();
 			parse = new Scanner(origin).useDelimiter("//");
 			origin = parse.next();
-//			System.out.println(def);
-//			System.out.println(bp);
-//			System.out.println(gene);
-//			System.out.println(year);
-//			System.out.println(origin);
-			vArray.add(new Virus(bp, gene, year, def, origin));
-//			System.out.println(vArray.toString());
-			
-		}
-		
-		
-		
-		boolean running = true;
-		
-		System.out.println(" __      ___                  _____        _        _                    \r\n" + 
-				" \\ \\    / (_)                |  __ \\      | |      | |                   \r\n" + 
-				"  \\ \\  / / _ _ __ _   _ ___  | |  | | __ _| |_ __ _| |__   __ _ ___  ___ \r\n" + 
-				"   \\ \\/ / | | '__| | | / __| | |  | |/ _` | __/ _` | '_ \\ / _` / __|/ _ \\\r\n" + 
-				"    \\  /  | | |  | |_| \\__ \\ | |__| | (_| | || (_| | |_) | (_| \\__ \\  __/\r\n" + 
-				"     \\/   |_|_|   \\__,_|___/ |_____/ \\__,_|\\__\\__,_|_.__/ \\__,_|___/\\___|\r\n" + 
-				"                                                                         \r\n" + 
-				"                                                                         ");
-		
-		System.out.println();
-		
-		
 
-		
-		while(running) {
-			VirusCollection vcollection = new VirusCollection(vArray);
-			enterCommand();
-			
-			Scanner s = new Scanner(System.in);
-			String input = s.next();
-			
-			if(input.equals("list")) {
-				
-				System.out.println();
-				System.out.println();
-				System.out.println("8888888888 d8b 888                        888                            888               888    \r\n" + 
-						"888        Y8P 888                        888                            888               888    \r\n" + 
-						"888            888                        888                            888               888    \r\n" + 
-						"8888888    888 888  .d88b.  .d8888b       888      .d88b.   8888b.   .d88888  .d88b.   .d88888    \r\n" + 
-						"888        888 888 d8P  Y8b 88K           888     d88\"\"88b     \"88b d88\" 888 d8P  Y8b d88\" 888    \r\n" + 
-						"888        888 888 88888888 \"Y8888b.      888     888  888 .d888888 888  888 88888888 888  888    \r\n" + 
-						"888        888 888 Y8b.          X88      888     Y88..88P 888  888 Y88b 888 Y8b.     Y88b 888    \r\n" + 
-						"888        888 888  \"Y8888   88888P'      88888888 \"Y88P\"  \"Y888888  \"Y88888  \"Y8888   \"Y88888    \r\n" );
-				for(int i = 0; i<dirFiles.length; i++) {
-					System.out.println(dirFiles[i].getName());
-				}
-				System.out.println();
-			} else if(input.equals("help")) {
-				System.out.println();
-				System.out.println();
-				
-				System.out.println("888    888          888              \r\n" + 
-						"888    888          888              \r\n" + 
-						"888    888          888              \r\n" + 
-						"8888888888  .d88b.  888 88888b.  d8b \r\n" + 
-						"888    888 d8P  Y8b 888 888 \"88b Y8P \r\n" + 
-						"888    888 88888888 888 888  888     \r\n" + 
-						"888    888 Y8b.     888 888 d88P d8b \r\n" + 
-						"888    888  \"Y8888  888 88888P\"  Y8P \r\n" + 
-						"                        888          \r\n" + 
-						"                        888          \r\n" + 
-						"                        888          \r\n" + 
-						"                                                 \r\n" + 
-						"888888 888888 888888 888888 888888\r\n");
-				System.out.println();
-				System.out.println("list: lists all files in directory.");
-				System.out.println();
-				System.out.println();
-			} else if(input.equals("Sort")) {
-				input = s.next();
-				if(input.equals("Gene")) {
-					vcollection.sortGene();
-				} else {
-					System.out.println("Invalid Input");
-				}
-			} else if(input.equals("Filter")) {
-				input = s.next();
-				if(input.equals("Gene")) {
-					input = s.next();
-					if(input.equals("DNA")) {
-						vcollection.filterGene("DNA");
-					} else if(input.equals("RNA")) {
-						vcollection.filterGene("RNA");
-					} else {
-						System.out.println("Invalid Input");
-					}
-				} else {
-					System.out.println("Invalid Input");
-				}
-			}
+			array.add(new Virus(bp, gene, year, def, origin));
 		}
-	}
-	
-	public static void enterCommand() {
-		System.out.println("Enter Command(Type 'help' for a list of commands): ");
+		System.out.println("parsed");
+		return array;
 	}
 }
--- src/VirusCollection.java
+++ src/VirusCollection.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 
 public class VirusCollection {
 	
-	public final ArrayList<Virus> readCollection;
-	
 	public ArrayList<Virus> collection;
 	
 	public VirusCollection(ArrayList<Virus> collection) {
-		readCollection = collection;
 		this.collection = collection;
 	}
 	
@@ -29,7 +26,7 @@ public class VirusCollection {
 	}
 	
 	public void filterGene(String s) {
-		collection = readCollection;
+		System.out.println("Pre Filter" + collection);
 		for(int i = 0; i<collection.size(); i++) {
 			if(s.equals("DNA") && collection.get(i).getGene().equals("RNA")) {
 				collection.remove(i);
@@ -39,10 +36,7 @@ public class VirusCollection {
 				i--;
 			}
 		}
-		for(int i = 0; i<collection.size(); i++) {
-//			System.out.println(collection.get(i).getDefinition());
-			System.out.println(collection.get(i).getGene());
-		}
+		printList();
 	}
 	
 	
@@ -65,7 +59,6 @@ public class VirusCollection {
 	
 	public void sortGene() {
 		System.out.println("Gene");
-		collection = readCollection;
 		
 		for(int i = 0; i<collection.size(); i++) {
 			System.out.println(i);
@@ -99,5 +92,35 @@ public class VirusCollection {
 ////			System.out.println(collection.get(i).getDefinition());
 //			System.out.println(collection.get(i).getGene());
 //		}
+	}
+	
+	private void printList() {
+		for(int i = 0; i<collection.size(); i++) {
+			System.out.println("__");
+			System.out.println("| " + collection.get(i).getDefinition());
+			System.out.println("| " + collection.get(i).getYear());
+			System.out.println("| " + collection.get(i).getReference());
+			System.out.println("| " + collection.get(i).getGene());
+//			System.out.println("| " + collection.g et(i).getOrigin());
+			System.out.println("__");
+		}
+		System.out.println("Post Filter" + collection);
+		collection = VirusClient.parse();
+		System.out.println("Post Parse" + collection);
+	}
+	
+	public void list() {
+		System.out.println();
+		System.out.println();
+		System.out.println("8888888888 d8b 888                        888                            888               888    \r\n" + 
+				"888        Y8P 888                        888                            888               888    \r\n" + 
+				"888            888                        888                            888               888    \r\n" + 
+				"8888888    888 888  .d88b.  .d8888b       888      .d88b.   8888b.   .d88888  .d88b.   .d88888    \r\n" + 
+				"888        888 888 d8P  Y8b 88K           888     d88\"\"88b     \"88b d88\" 888 d8P  Y8b d88\" 888    \r\n" + 
+				"888        888 888 88888888 \"Y8888b.      888     888  888 .d888888 888  888 88888888 888  888    \r\n" + 
+				"888        888 888 Y8b.          X88      888     Y88..88P 888  888 Y88b 888 Y8b.     Y88b 888    \r\n" + 
+				"888        888 888  \"Y8888   88888P'      88888888 \"Y88P\"  \"Y888888  \"Y88888  \"Y8888   \"Y88888    \r\n" );
+		printList();
+		System.out.println();
 	}
 }
